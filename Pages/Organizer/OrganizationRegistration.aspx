a0fdb2616c7564d52b9dfe138fc5601b2a1fdd97
Stephen Tongol <spvtongol@gmail.com>
2022-05-28 20:49:14 +0800

    Layout changes

--- Pages/Organizer/OrganizationRegistration.aspx
+++ Pages/Organizer/OrganizationRegistration.aspx
@@ -21,95 +21,57 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <div class="form-body">
-        <div class="wrapper">
-            <p class="header">Register</p>
-            <div class="form">
-                <div>
-                    <asp:Label ID="lbl_Name" runat="server" Text="Organization Name"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvName" runat="server" ErrorMessage="*" ControlToValidate="tb_Name" CssClass="validator"></asp:RequiredFieldValidator>
+        <p class="header">Register</p>
+        <div class="form-section">
+            <div id="category-section" class="sub-header" onclick="toggleFormSections(this)">
+                <p style="width: 90%;">Information</p>
+                <p id="category-section-indicator" style="float: right; margin-right:15px;">-</p>
+            </div>
+
+            <div id="category-section-fields" class="form-fields">
+                <div class="form-field">
+                     <p>Name<asp:RequiredFieldValidator ID="rfvName" runat="server" ErrorMessage="*" ControlToValidate="tb_Name" CssClass="validator"></asp:RequiredFieldValidator></p>
+                     <asp:TextBox ID="tb_Name" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
                 </div>
-                <div>
-                    <asp:TextBox ID="tb_Name" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
+                <div class="form-field">
+                     <p>Logo<asp:RequiredFieldValidator ID="rfvLogo" runat="server" ErrorMessage="*" ControlToValidate="tb_Logo" CssClass="validator"></asp:RequiredFieldValidator></p>
+                     <asp:TextBox ID="tb_Logo" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
                 </div>
-                <div>
-                    <asp:Label ID="lbl_Logo" runat="server" Text="Logo"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvLogo" runat="server" ErrorMessage="*" ControlToValidate="tb_Logo" CssClass="validator"></asp:RequiredFieldValidator>
+                <div class="form-field">
+                     <p>Acronym<asp:RequiredFieldValidator ID="rfv_Acronym" runat="server" ErrorMessage="*" ControlToValidate="tb_Acronym" CssClass="validator"></asp:RequiredFieldValidator></p>
+                     <asp:TextBox ID="tb_Acronym" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
                 </div>
-                <div>
-                    <asp:TextBox ID="tb_Logo" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_Acronym" runat="server" Text="Organization Acronym"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvAcronym" runat="server" ErrorMessage="*" ControlToValidate="tb_Acronym" CssClass="validator"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:TextBox ID="tb_Acronym" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_Type" runat="server" Text="Type"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvType" runat="server" ErrorMessage="*" ControlToValidate="tb_Type" CssClass="validator"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:TextBox ID="tb_Type" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_Contact" runat="server" Text="Contact"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvContact" runat="server" ErrorMessage="*" ControlToValidate="tb_Contact" CssClass="validator"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:TextBox ID="tb_Contact" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_Mission" runat="server" Text="Mission"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvMission" runat="server" ErrorMessage="*" ControlToValidate="tb_Mission" CssClass="validator"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:TextBox ID="tb_Mission" runat="server" Width="500px" Height="120px" CssClass="text-boxs" style="overflow:auto; resize:none; " TextMode="MultiLine"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_Vision" runat="server" Text="Vision"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvVision" runat="server" ErrorMessage="*" ControlToValidate="tb_Vision" CssClass="validator"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:TextBox ID="tb_Vision" runat="server" Width="500px" Height="120px" CssClass="text-boxs" class ="textbox" style="overflow:auto; resize:none;" TextMode="MultiLine"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_College" runat="server" Text="College"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvCollege" runat="server" ErrorMessage="*" ControlToValidate="ddl_College" CssClass="validator" InitialValue="Choose"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:DropDownList ID="ddl_College" runat="server" Width ="100px" CssClass="dropdown-lists">
+                <div class="form-field">
+                     <p>Type<asp:RequiredFieldValidator ID="rfv_Type" runat="server" ErrorMessage="*" ControlToValidate="ddl_Type" CssClass="validator" InitialValue="Choose"></asp:RequiredFieldValidator></p>
+                     <asp:DropDownList ID="ddl_Type" runat="server" CssClass="dropdown-lists">
                         <asp:ListItem Selected="True">Choose</asp:ListItem>
-                        <asp:ListItem>CAS</asp:ListItem>
-                        <asp:ListItem>CCIS</asp:ListItem>
-                        <asp:ListItem>MITL</asp:ListItem>
-                        <asp:ListItem>CMET</asp:ListItem>
+                        <asp:ListItem>Non-Academic</asp:ListItem>
+                        <asp:ListItem>Academic</asp:ListItem>
                     </asp:DropDownList>
                 </div>
-                <div>
-                    <asp:Label ID="lbl_AdviserNum" runat="server" Text="Adviser Employee Number"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvAdviserNum" runat="server" ErrorMessage="*" ControlToValidate="tb_AdviserNum" CssClass="validator"></asp:RequiredFieldValidator>
+                <div class="form-field">
+                     <p>Contact<asp:RequiredFieldValidator ID="rfv_Contact" runat="server" ErrorMessage="*" ControlToValidate="tb_Contact" CssClass="validator"></asp:RequiredFieldValidator></p>
+                     <asp:TextBox ID="tb_Contact" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
                 </div>
-                <div>
-                    <asp:TextBox ID="tb_AdviserNum" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_VicePresNum" runat="server" Text="Vice President Student Number"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvVicePresNum" runat="server" ErrorMessage="*" ControlToValidate="tb_VicePresNum" CssClass="validator"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:TextBox ID="tb_VicePresNum" runat="server" Width="500px" CssClass="text-boxs"></asp:TextBox>
-                </div>
-                <div>
-                    <asp:Label ID="lbl_Members" runat="server" Text="Members"></asp:Label>
-                    <asp:RequiredFieldValidator ID="rfvMembers" runat="server" ErrorMessage="*" ControlToValidate="tb_Members" CssClass="validator"></asp:RequiredFieldValidator>
-                </div>
-                <div>
-                    <asp:TextBox ID="tb_Members" runat="server" Width="500px" Height="120px" CssClass="text-boxs" style="overflow:auto; resize:none;" TextMode="MultiLine" ></asp:TextBox>
-                </div>
-                <div style = "margin: 15px auto">
-                    <asp:Button ID="btn_Register" runat="server" Text="Register" CssClass="main-button"/>
-                </div>
+             </div>
+
+            <%--<div>
+                <asp:Label ID="lbl_College" runat="server" Text="College"></asp:Label>
+                <asp:RequiredFieldValidator ID="rfvCollege" runat="server" ErrorMessage="*" ControlToValidate="ddl_College" CssClass="validator" InitialValue="Choose"></asp:RequiredFieldValidator>
+            </div>
+            <div>
+                <asp:DropDownList ID="ddl_College" runat="server" CssClass="dropdown-lists">
+                    <asp:ListItem Selected="True">Choose</asp:ListItem>
+                    <asp:ListItem>SHS</asp:ListItem>
+                    <asp:ListItem>CAS</asp:ListItem>
+                    <asp:ListItem>CCIS</asp:ListItem>
+                    <asp:ListItem>MITL</asp:ListItem>
+                    <asp:ListItem>CMET</asp:ListItem>
+                </asp:DropDownList>
+            </div>--%>
+         
+            <div style = "margin: 15px auto">
+                <asp:Button ID="btn_Register" runat="server" Text="Register" CssClass="main-button"/>
             </div>
         </div>
     </div>
--- Pages/Organizer/OrganizationRegistration.aspx.designer.cs
+++ Pages/Organizer/OrganizationRegistration.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace MalayanEventHub.Layouts
     {
 
         /// <summary>
-        /// lbl_Name control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Name;
-
-        /// <summary>
         /// rfvName control.
         /// </summary>
         /// <remarks>
@@ -40,15 +31,6 @@ namespace MalayanEventHub.Layouts
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox tb_Name;
-
-        /// <summary>
-        /// lbl_Logo control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Logo;
 
         /// <summary>
         /// rfvLogo control.
@@ -69,22 +51,13 @@ namespace MalayanEventHub.Layouts
         protected global::System.Web.UI.WebControls.TextBox tb_Logo;
 
         /// <summary>
-        /// lbl_Acronym control.
+        /// rfv_Acronym control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Acronym;
-
-        /// <summary>
-        /// rfvAcronym control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvAcronym;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfv_Acronym;
 
         /// <summary>
         /// tb_Acronym control.
@@ -96,49 +69,31 @@ namespace MalayanEventHub.Layouts
         protected global::System.Web.UI.WebControls.TextBox tb_Acronym;
 
         /// <summary>
-        /// lbl_Type control.
+        /// rfv_Type control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Type;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfv_Type;
 
         /// <summary>
-        /// rfvType control.
+        /// ddl_Type control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvType;
+        protected global::System.Web.UI.WebControls.DropDownList ddl_Type;
 
         /// <summary>
-        /// tb_Type control.
+        /// rfv_Contact control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox tb_Type;
-
-        /// <summary>
-        /// lbl_Contact control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Contact;
-
-        /// <summary>
-        /// rfvContact control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvContact;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfv_Contact;
 
         /// <summary>
         /// tb_Contact control.
@@ -148,168 +103,6 @@ namespace MalayanEventHub.Layouts
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox tb_Contact;
-
-        /// <summary>
-        /// lbl_Mission control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Mission;
-
-        /// <summary>
-        /// rfvMission control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvMission;
-
-        /// <summary>
-        /// tb_Mission control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox tb_Mission;
-
-        /// <summary>
-        /// lbl_Vision control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Vision;
-
-        /// <summary>
-        /// rfvVision control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvVision;
-
-        /// <summary>
-        /// tb_Vision control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox tb_Vision;
-
-        /// <summary>
-        /// lbl_College control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_College;
-
-        /// <summary>
-        /// rfvCollege control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvCollege;
-
-        /// <summary>
-        /// ddl_College control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddl_College;
-
-        /// <summary>
-        /// lbl_AdviserNum control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_AdviserNum;
-
-        /// <summary>
-        /// rfvAdviserNum control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvAdviserNum;
-
-        /// <summary>
-        /// tb_AdviserNum control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox tb_AdviserNum;
-
-        /// <summary>
-        /// lbl_VicePresNum control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_VicePresNum;
-
-        /// <summary>
-        /// rfvVicePresNum control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvVicePresNum;
-
-        /// <summary>
-        /// tb_VicePresNum control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox tb_VicePresNum;
-
-        /// <summary>
-        /// lbl_Members control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbl_Members;
-
-        /// <summary>
-        /// rfvMembers control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvMembers;
-
-        /// <summary>
-        /// tb_Members control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox tb_Members;
 
         /// <summary>
         /// btn_Register control.
