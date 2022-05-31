e6aacf21f51e8c0d1a7ad0964af46095602df008
Stephen Tongol <spvtongol@gmail.com>
2022-05-31 16:43:37 +0800

    Edited query to load organization

--- Pages/Organizer/OrganizationsOrganizer.aspx.cs
+++ Pages/Organizer/OrganizationsOrganizer.aspx.cs
@@ -28,12 +28,12 @@ namespace MalayanEventHub.Layouts
 
         private void Load_Organization()
         {
-            string query = "SELECT e.*, e_req.*,req.*  FROM EventTBL as e " +
-            "INNER JOIN EventRequestTBL as e_req " +
-            "ON e.eventID = e_req.eventID " +
-            "INNER JOIN RequestTBL as req " +
-            "ON e_req.requestID = req.requestID " +
-            $"WHERE e.eventID = {eventId}; ";
+            string query = "SELECT OrganizationTBL.organizationID," +
+                " OrganizationTBL.organizationName, OrganizationTBL.organizationType," +
+                " OrganizationTBL.college FROM OrganizationTBL" +
+                " INNER JOIN MemberTBL ON OrganizationTBL.organizationID = MemberTBL.organizationID" +
+                $" WHERE MemberTBL.userId = {userID}";
+
         }
 
         protected void btnRegister_Click(object sender, EventArgs e)
