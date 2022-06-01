90957ec0db875313d1343fa4bbbcb4a81f1a819a
karlopenguini <karlobpalisoc@gmail.com>
2022-06-01 20:00:33 +0800

    stuff

--- Layouts/Common/Admin/OrganizationView.master.designer.cs
+++ Layouts/Common/Admin/OrganizationView.master.designer.cs
@@ -24,6 +24,51 @@ namespace MalayanEventHub.Layouts.Common.Admin
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head2;
 
         /// <summary>
+        /// lbl_org_name control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_org_name;
+
+        /// <summary>
+        /// lbl_org_type control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_org_type;
+
+        /// <summary>
+        /// lbl_org_college control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_org_college;
+
+        /// <summary>
+        /// lbl_org_status control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_org_status;
+
+        /// <summary>
+        /// btn_settings control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_settings;
+
+        /// <summary>
         /// ContentPlaceHolder2 control.
         /// </summary>
         /// <remarks>
