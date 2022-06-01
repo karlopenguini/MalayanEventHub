c77b8fd599811cd26ad1707305496fb3562f1149
requintina.marvin16@gmail.com <requintina.marvin16@gmail.com>
2022-06-01 19:49:56 +0800

    practice

---
+++ Pages/Admin/ViewOrganizations.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layouts/AdminView.Master" AutoEventWireup="true" CodeBehind="ViewOrganizations.aspx.cs" Inherits="MalayanEventHub.Layouts.WebForm4" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+     <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
+    <link href="../../Styles/Admin/StyleSheetViewOrgs.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="main">
+            <div class="auto-style1">
+                <asp:DropDownList ID="ddlCollege" runat="server" AutoPostBack="True">
+                    <asp:ListItem>CAS</asp:ListItem>
+                    <asp:ListItem>CCIS</asp:ListItem>
+                    <asp:ListItem>ETYCB</asp:ListItem>
+                    <asp:ListItem>MITL</asp:ListItem>
+                    <asp:ListItem>CMET</asp:ListItem>
+                </asp:DropDownList>
+                <asp:DataList ID="DataList1" runat="server" CellPadding="20" RepeatDirection="Horizontal" RepeatColumns="3">
+                    <ItemTemplate>
+                        <div class="card">
+                            <img src="ImagesLogo/1.png" />
+                            <div class="info">
+                               <a href="<%# FormatUrl( "OrgDetails.aspx",(int) Eval("organizationID")) %>" class="btn">Details</a>
+                            </div>
+                        </div>
+                    </ItemTemplate>
+                    <SeparatorTemplate></SeparatorTemplate>
+                </asp:DataList>
+            </div>
+        </div>
+</asp:Content>
---
+++ Pages/Admin/ViewOrganizations.aspx.cs
@@ -0,0 +1,56 @@
+﻿using MalayanEventHub.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MalayanEventHub.Layouts
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+
+        DatabaseHandler dbHandler;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                dbHandler = new DatabaseHandler();
+                LoadOrganizations();
+            }
+        }
+
+        protected void LoadOrgData()
+        {
+            string query = "Select * from OrganizationTBL where college = "
+        }
+            
+
+
+            //{
+
+            //    SqlConnection connect = new SqlConnection("Server = tcp:mcleventhub.database.windows.net, 1433; Initial Catalog = MalayanEventHubDB; Persist Security Info = False; User ID = group2; Password = !Malayaneventhub1234; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+            //    SqlCommand cmd = new SqlCommand("select * from OrganizationTBL where college = @college", connect);
+            //    connect.Open();
+            //    cmd.Parameters.AddWithValue("@college", ddlCollege.SelectedItem.Text.ToString());
+            //    SqlDataReader sdr = cmd.ExecuteReader();
+            //    if (sdr.HasRows)
+            //    {
+            //        DataList1.DataSource = sdr;
+            //        DataList1.DataBind();
+            //    }
+            //    else
+            //    {
+            //        DataList1.DataSource = "";
+            //        DataList1.DataBind();
+            //    }
+            //    connect.Close();
+            //}
+        protected string FormatUrl(string site, int orgID)
+        {
+            return site + "?organizationID=" + orgID;
+        }
+    }
+}
