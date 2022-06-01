40bb604030ac3a7a203cecdf57ed1179173188e8
requintina.marvin16@gmail.com <requintina.marvin16@gmail.com>
2022-06-02 03:09:53 +0800

    draft

--- Pages/Admin/OrgReview.aspx.cs
+++ Pages/Admin/OrgReview.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MalayanEventHub.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,28 +10,95 @@ namespace MalayanEventHub.Layouts
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
+        string organizationID;
+        string organizationName;
+        string organizationAcronym;
+        string organizationType;
+        string organizationAdviser;
+        string organizationContact;
+        string organizationStatus;
+        string mission;
+        string vision;
+        string college;
+        string logo;
+        string requestID;
+
+        DatabaseHandler dbHandler = new DatabaseHandler();
+        
+
+        //di nag rereference dito yung mga textbox na ginawa ko
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Page.IsPostBack)
-            {
-                EventData SampleEvent = new EventData
-                {
-                    MemberName = "Marvin Allen N. Requintina"
-                };
-                List<EventData> SampleEvents = new List<EventData>();
-
-                for (int i = 0; i < 5; i++)
-                {
-                    SampleEvents.Add(SampleEvent);
-                }
-
-                member_lists.DataSource = SampleEvents;
-                member_lists.DataBind();
-            }
+            organizationID = Request.QueryString["organizationID"];
         }
-        public class EventData
+       
+
+        protected void LoadOrganizationData()
         {
-            public string MemberName { get; set; }
+            //kulang ng  query for vice, treasurer, secretary
+            string query = $"SELECT * FROM OrganizationTBL WHERE organizationID = {organizationID}";
+            Dictionary<string, string> data = dbHandler.RetrieveData(query)[0];
+
+            organizationName = data["organizationName"];
+            organizationAcronym = data["organizationAcronym"];
+            organizationType = data["organizationType"];
+            organizationAdviser = data["organizationAdviser"];
+            organizationContact = data["organizationContact"];
+            organizationStatus = data["organizationStatus"];
+            //vicenName=data['di ko pa alam'];
+            //treasusrerName=data['di ko pa alam'];
+            //secretaryName=data['di ko pa alam'];
+            mission = data["mission"];
+            vision = data["vision"];
+            college = data["college"];
+
+            tb_orgname.Text = organizationName;
+          //tb_vice.Text = viceName;
+          //tb_treasurer.Text = treasurerName;
+          //tb_secretary.Text= secretaryName;
+            tb_acronym.Text = organizationAcronym;
+            tb_type.Text = organizationType;
+            tb_contact.Text = organizationContact;
+            tb_adviser.Text = organizationAdviser;
+            tb_mission.Text = mission;
+            tb_vision.Text = vision;
+            tb_college.Text = college;
+        }
+
+        protected string GetRequestID()
+        {
+
+            string query =
+                "SELECT OrganizationRequestTBL.requestID" +
+                " FROM OrganizationRequestTBL" +
+                " INNER JOIN OrganizationTBL ON OrganizationTBL.organizationID = OrganizationRequestTBL.organizationID" +
+                $"WHERE {organizationID} = OrganizationRequestTBL.organizationID;";
+
+            requestID = dbHandler.RetrieveData(query)[0]["requestID"];
+            return requestID;
+        }
+
+
+        //wala akong feedback. Accept and Reject lang.
+        protected void Btn_accept_Click(object sender, EventArgs e)
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + requestID + "');", true);
+            string now = DateTime.Now.ToString("dd-MM-yy hh:mm:ss");
+            string query =
+                "UPDATE RequestTBL" +
+                $" SET requestStatus = 'Active', modified='{now}'" +
+                $" WHERE '{requestID}' = RequestTBL.requestID";
+            dbHandler.ExecuteUpdateQuery(query);
+        }
+
+        protected void Btn_reject_Click(object sender, EventArgs e)
+        {
+            string now = DateTime.Now.ToString("dd-MM-yy hh:mm:ss");
+            string query =
+                "UPDATE RequestTBL" +
+                $" SET requestStatus = 'Rejected', modified='{now}'" +
+                $" WHERE '{requestID}' = RequestTBL.requestID";
+            dbHandler.ExecuteUpdateQuery(query);
         }
     }
 }
